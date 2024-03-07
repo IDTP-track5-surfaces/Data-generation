ede1a8706f7fcecc91bcb01df4ac520e5cee0bc6
Imre Atmodimedjo <i.s.atmodimedjo@student.tue.nl>
2024-03-07 16:20:34 +0100

    Add refraction equations with dot product

--- utils.py
+++ utils.py
@@ -4,32 +4,46 @@ from scipy.ndimage import sobel
 
    
 # Function to perform ray tracing
-def raytracing_im_generator_ST(im_rgb, this_depth, n1, n2, selector = True):
+def raytracing_im_generator_ST(im_rgb, depth_map, n1, n2, selector = True):
     step = 1
-    h, w, dim = im_rgb.shape
+    h, w, channels = im_rgb.shape
 
+    # Generate normal map from depth map
+    Gx = sobel(depth_map, axis=0)
+    Gy = sobel(depth_map, axis=1)
 
-    Gx = sobel(this_depth, axis=0)
-    Gy = sobel(this_depth, axis=1)
-
-    normal_ori = np.ones_like(im_rgb)
+    # Create normal vectors
+    normal_ori = np.ones((h,w, 3))
     normal_ori[:, :, 0] = -Gx
     normal_ori[:, :, 1] = -Gy
     
+    # Normalize normal vectors
     norm = np.sqrt(Gx**2 + Gy**2 + 1)
     normal = normal_ori / norm[..., None]
 
+    # Create incident vectors
+    s1 = np.zeros_like(normal)
+    s1[:, :, 2] = -1
+    
     if selector: # refraction
-        s1 = np.zeros_like(normal)
-        s1[:, :, 2] = -1
         s2 = refraction(normal, s1, n1, n2)
     else: # reflection 
         print("Not implemented")
-        s1 = np.zeros_like(normal)
+        
+        # Rotation angle
+        incident_angle = np.pi/3 # 60 degrees
+        theta = np.pi/2 - incident_angle
+
+        # Rotation matrix
+        R = np.array([[1, 0, 0],[0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+
+        # Rotated incident vectors
+        s1 = np.einsum('ij,pqj->pqi', R, s1)
+        
         s2 = reflection(normal, s1) # NOT IMPLEMENTED
     
 
-    a = this_depth / s2[:, :, 2]
+    a = depth_map / s2[:, :, 2]
     x_c = np.round(a * s2[:, :, 0] / step, 2)
     y_c = np.round(a * s2[:, :, 1] / step, 2)
 
@@ -50,6 +64,17 @@ def refraction(normal, s1, n1, n2):
 
     return s2_normalized
 
+def refraction_wikipedia(normal, s1, n1, n2):
+    n = n1/n2
+    
+    cos1 = np.einsum('ijk, ijk->ij', -normal, s1)
+    cos2 = np.sqrt(1-normal**2*(1-cos1**2))
+    
+    s2 = n*s1 + (n*cos1-cos2)*normal
+    
+    s2_normalized = s2/np.linalg.norm(s2, axis =2)[..., None]
+    
+    return s2_normalized
 
 # def reflection(normal, s1):
 #     dot_n_s = normal[:, :, 0] * s1[:, :, 0] + normal[:, :, 1] * s1[:, :, 1] + normal[:, :, 2] * s1[:, :, 2]
@@ -59,6 +84,16 @@ def refraction(normal, s1, n1, n2):
 #     return s2
 
 def deform_image(img, warp_map):
+    """_summary_
+
+    Args:
+        img (_type_): Original reference pattern
+        warp_map (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    
     h, w, nChannel = img.shape
     # Create meshgrid for the original coordinates
     X, Y = np.meshgrid(np.arange(1, w + 1), np.arange(1, h + 1))
@@ -91,8 +126,8 @@ def deform_image(img, warp_map):
 
 def generate_example_depth_map():
     xymax = 10
-    A = np.zeros((900,900))
+    depth_map = np.zeros((900,900))
     for i,x in enumerate(np.linspace(-xymax, xymax, num=900)):
         for j,y in enumerate(np.linspace(-xymax, xymax, num=900)):
-            A[i,j] = 20*np.sin(-(x**2 + y**2)/10)
-    return A
+            depth_map[i,j] = 20*np.sin(-(x**2 + y**2)/10)
+    return depth_map
