c18bd7757424796817f794b121a142f6c2817925
Imre Atmodimedjo <i.s.atmodimedjo@student.tue.nl>
2024-05-24 13:37:59 +0200

    Adjust for new naming scheme

--- data_set.py
+++ data_set.py
@@ -23,45 +23,45 @@ def create_depth_and_normal_maps(w=128, fps = 24):
     X, Y = np.meshgrid(x, y)
     seq = 0
     
-    # for dx in np.linspace(-13e-3,13-3, 10):
-    #     for dy in np.linspace(-13e-3,13-3, 10):
-    for wave_width in np.linspace(0.5, 3, 45):
-        for i, ta in enumerate(np.linspace(0.5, 10, int(fps*10))):
-            depth_map = Puff_profile(X, Y, ta, width=wave_width) 
-            
-            # Apply 2D cubic spline interpolation
-            depth_map_smooth = RectBivariateSpline(x, y, depth_map)
-            
-            # Compute the gradients of depth_map_smooth with respect to x and y
-            Gx = depth_map_smooth.partial_derivative(1,0)(x,y) 
-            Gy = depth_map_smooth.partial_derivative(0,1)(x,y)
+    for wave_width in np.linspace(0.5, 3, 9):
+        for wave_depth in np.linspace(-0.003, -0.008, num=6):
+            for i, ta in enumerate(np.arange(start=0.5, stop=10, step=1/fps)):
+                depth_map = Puff_profile(X, Y, ta, depth=wave_depth, width=wave_width) 
                 
-            # Create normal vectors
-            normal_ori = np.ones((w, w, 3)) # Default reference image size
-            normal_ori[:, :, 0] = -Gx
-            normal_ori[:, :, 1] = -Gy
-            
-            # Normalize normal vectors
-            norm = np.sqrt(Gx**2 + Gy**2 + 1)
-            normal = normal_ori / norm[..., None]
+                # # Apply 2D cubic spline interpolation
+                # depth_map_smooth = RectBivariateSpline(x, y, depth_map)
+                
+                # # Compute the gradients of depth_map_smooth with respect to x and y
+                # Gx = depth_map_smooth.partial_derivative(1,0)(x,y) 
+                # Gy = depth_map_smooth.partial_derivative(0,1)(x,y)
+                
+                Gx, Gy = grad_puff_profile(X, Y, ta, depth=wave_depth, width=wave_width)
+                    
+                # Create normal vectors
+                normal_ori = np.ones((w, w, 3)) # Default reference image size
+                normal_ori[:, :, 0] = -Gx
+                normal_ori[:, :, 1] = -Gy
+                
+                # Normalize normal vectors
+                norm = np.sqrt(Gx**2 + Gy**2 + 1)
+                normal = normal_ori / norm[..., None]
 
-            # Adjust height
-            depth_map = depth_map + 2e-2  
+                # Adjust height
+                depth_map = depth_map + 1e-2  # in meters
             
-            # Save normal and depth map
-            file_name_depth = f"depth_map_seq{seq}_f{i}" 
-            file_name_normal = f"normal_map_seq{seq}_f{i}" 
-            np.save(os.path.join(DATA_SETS_DIR, "depth", file_name_depth), depth_map)
-            np.save(os.path.join(DATA_SETS_DIR, "normal", file_name_normal), normal)
-        # Increase sequence number for different simulation
-        seq +=1
+                # Save normal and depth map
+                file_name_depth = f"depth_seq{seq}-f{i}" 
+                file_name_normal = f"normal_seq{seq}-f{i}" 
+                np.save(os.path.join(DATA_SETS_DIR, "depth", file_name_depth), depth_map)
+                np.save(os.path.join(DATA_SETS_DIR, "normal", file_name_normal), normal)
+            # Increase sequence number for different simulation
+            seq +=1
     return
 
 def create_warp_maps(n1 = 1, n2 = 1.33):
     """This function creates a warp map corresponding to a depth map and normalmap
     
     Args:
-        phase {"train", "test", "validation"}: Select wether the data is used for training / testing or validation.
         n1 (float, optional): Refractive index of the incident medium. Defaults to 1 (air).
         n2 (float, optional): Refractive index of refractive medium. Defaults to 1.33 (water).
     """
@@ -75,17 +75,16 @@ def create_warp_maps(n1 = 1, n2 = 1.33):
         
         for transparent in ["reflection", "refraction"]:
             warp_map = raytracing_im_generator_ST(normal, depth_map, transparent, n1=n1, n2=n2)
-            file_name = "warp_map" + file_index
+            file_name = "warp_seq" + file_index
             np.save(os.path.join(DATA_SETS_DIR, "warp_map", transparent, file_name), warp_map)
             
                     
-def create_warped_images(image, image_name, gray_scale=False):
+def create_warped_images(image, gray_scale=False):
     """This function warps a image according to a warp map.
     
     Args:
         image (ndarray): image with 3 channels for the RGB values.
         image_name (string): name of the image.
-        train_or_test {"train", "test"}: Select wether the data is used for training or testing. Defaults to "train".
         refract_or_reflect {"refraction", "reflection"}: Select if refraction or reflection model is used. Defaults to "refraction".
     """
     if gray_scale:
@@ -101,7 +100,7 @@ def create_warped_images(image, image_name, gray_scale=False):
         warp_dir = os.path.join(DATA_SETS_DIR, "warp_map", transparent)
         for file in os.listdir(warp_dir):
             warp_map = np.load(os.path.join(warp_dir, file))
-            image_name_save = image_name + file[8:-3] + "png" # check this
+            image_name = file[5:-4] + ".png" # check this
         
             # Deform image
             rgb_deformation = deform_image(rgb, warp_map)
@@ -111,21 +110,17 @@ def create_warped_images(image, image_name, gray_scale=False):
             
             # Save image
             image_deformation = np.array(rgb_deformation * 255, dtype=np.uint8)
-            imwrite(os.path.join(DATA_SETS_DIR, transparent, image_name_save), image_deformation)
+            imwrite(os.path.join(DATA_SETS_DIR, transparent, image_name), image_deformation)
             
             
 if __name__ == "__main__":
-    create_directory_structure()
-    create_depth_and_normal_maps(w=128, fps=4.5)
-    create_warp_maps(n1=1, n2=1.33) 
+    create_directory_structure()  
+    create_depth_and_normal_maps(fps=12)
+    create_warp_maps() 
     
-    # Create deformed image for each reference pattern
-    for file in os.listdir('reference_patterns'):
-        file_name = os.path.splitext(file)[0]
-        file_path = os.path.join('reference_patterns', file)
-        image  = imread(file_path)
-
-        create_warped_images(image, file_name, gray_scale=True)
-        print(f"{file_name} is finished")
-        
+    # Create deformed image for each reference pattern  
+    # file_path = os.path.join('reference_patterns', file_name)
+    # image  = imread(file_path)
+    # create_warped_images(image)
+    
     print("Data generation is finished")
