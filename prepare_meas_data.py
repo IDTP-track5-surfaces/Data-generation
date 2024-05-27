55ca627f605156773ebe655dfe46aeb273f8e8e3
Imre Atmodimedjo <i.s.atmodimedjo@student.tue.nl>
2024-05-27 20:14:58 +0200

    Fix bug in preprocessing of laser

--- prepare_meas_data.py
+++ prepare_meas_data.py
@@ -3,8 +3,9 @@ import cv2
 import warnings
 import numpy as np
 import pandas as pd
-
 from scipy.optimize import curve_fit
+
+import matplotlib.pyplot as plt
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 MEAS_DIR = os.path.join(ROOT_DIR, "real_data")
@@ -12,12 +13,9 @@ DATA_DIR = os.path.join(MEAS_DIR, "data_sets")
 IMAGE_DIR = os.path.join(MEAS_DIR, "photos")
 LASER_DIR = os.path.join(MEAS_DIR, "laser")
 
-def r(x,y):
-    return np.sqrt(x**2 + y**2)
-
 def puff_fit(r, a,b,c,d):
     # scaled logistic function describing surface deformation
-    return d + (c) / (1 + np.exp(a - b * r**2))
+    return d + c / (1 + np.exp(a - b * r**2))
 
 def preprocess_image(file_loc):
     image = cv2.imread(file_loc)
@@ -33,7 +31,6 @@ def preprocess_image(file_loc):
     # Define where these points should map to in the orthographic view
     pts_dst = np.array([[402, 97], [575, 97], [402, 248], [575, 248]], dtype='float32')
 
-    # Compute the perspective transform matrix
     M = cv2.getPerspectiveTransform(pts_src, pts_dst)
 
     # Apply the perspective transformation
@@ -75,9 +72,11 @@ def preprocess_depth_array(depth_array, bounds = (-22, 28)):
     lb, ub = bounds
     max_radius = np.abs((lb-ub)/np.sqrt(2))
     
-    data = depth_array[np.where(depth_array[:,0]>lb)]
-    data = depth_array[np.where(depth_array[:,0]<ub)]
-
+    data = depth_array.copy()
+    
+    data = data[np.where(data[:,0]>lb)]
+    data = data[np.where(data[:,0]<ub)]
+    
     data[:,0] = np.flip(np.abs(data[:,0] - ub))
     data[:,1] = np.flip(data[:,1])
     
@@ -91,40 +90,44 @@ def preprocess_depth_array(depth_array, bounds = (-22, 28)):
     
     return (x_data, y_data), max_radius
 
-def parameter_fit(data, func=puff_fit, maxfev=50000):
+def parameter_fit(data, maxfev=50000):
     x_data, y_data = data
-    parameters, covariance = curve_fit(func, x_data, y_data, maxfev=maxfev)
-    return parameters, func
+    
+    parameters, covariance = curve_fit(puff_fit, x_data, y_data, maxfev=maxfev)
+    return parameters
 
-def create_depth(width, parameters, func=puff_fit):
+def create_depth(width, parameters):
     xy_range = np.linspace(-width, width, 128)
     X, Y = np.meshgrid(xy_range, xy_range)
-    mesh = r(X, Y)
+    mesh = np.sqrt(X**2 + Y**2)
     # mesh /= 1000
     
     a,b,c,d = parameters
-    depth = puff_fit(mesh,a,b,c,d)
+    depth = puff_fit(mesh, a,b,c,d)
     return depth
     
 def make_depth_maps(width, save_location = DATA_DIR, laser_directory = LASER_DIR):
 
-    
     for pressure in os.listdir(laser_directory):
         pressure_directory = os.path.join(laser_directory, pressure)
         depth_array = read_csv_laser_measurements(pressure_directory)
         data, max_radius = preprocess_depth_array(depth_array)
-        parameters, func = parameter_fit(data)
+        parameters = parameter_fit(data)
         
+        # make sure depth map is in correct domain
         if width > max_radius:
-            warnings.warn('width is larger than the maximum radius. Therefore, width=max_radius.') 
+            warnings.warn(f'width is larger than the maximum radius (max_radius = {max_radius}). Therefore, width=max_radius.') 
             width = max_radius
             
-        depth = create_depth(width, parameters, func=func)
+        depth = create_depth(width, parameters)
         file_loc = os.path.join(save_location, pressure, "depth.npy")
+        
         np.save(file_loc, depth)
     
-    
+    plt.plot(depth[64,:])
+    plt.show()
+
 if __name__ == "__main__":
     os.makedirs(DATA_DIR, exist_ok=True)
     create_image_data()
-    make_depth_maps(20e-3)
+    make_depth_maps(0.0195)
